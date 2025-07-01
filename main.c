dedda2dae565ee575c8c0e15ddc2e40481a38496
Aleksander Smaga <165085833+asmagaa@users.noreply.github.com>
2025-07-01 23:48:35 +0200

    implementing text viewer

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 // Includes
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <ctype.h>
-#include <errno.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <termios.h>
+#include <unistd.h>
 
 // Defines
 #define KILO_VERSION "0.0.1"
@@ -25,10 +26,17 @@ enum editorKey {
 };
 
 // Data
+typedef struct erow {
+    int size;
+    char *chars;
+} erow;
+
 struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
 };
 
@@ -144,6 +152,18 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+// file i/o
+void editorOpen() {
+    char *line = "Hello, world!";
+    ssize_t linelen = 13;
+
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+}
+
 // Append buffer
 struct abuf {
     char *b;
@@ -168,6 +188,9 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
+        if (y >= E.numrows) {
+            
+        }
         if (y == E.screenrows / 3) {
             char welcome[80];
             int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
@@ -276,6 +299,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
 
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
@@ -283,21 +307,11 @@ void initEditor() {
 int main() {
     enableRawMode();
     initEditor();
+    editorOpen();
 
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
-        //char c = '\0';
-
-        //if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-
-        /*if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q')) break; */
     }
-    
     return 0;
 }
